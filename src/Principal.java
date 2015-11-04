29e55f06c75960c540252085bcd29e26fa198970
aletaschner <aletaschner@gmail.com>
2015-11-04 20:26:22 -0200

    Teste sdsdf

--- src/Principal.java
+++ src/Principal.java
@@ -2,11 +2,7 @@
 public class Principal {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
 		System.out.print("Diego gay");
-=======
-		System.out.print("Hello World testando");
->>>>>>> origin/master
 
 	}
 
